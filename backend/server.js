a0496fae1a265058ccb117105a6ca735a7dc9783
Sundas-Razzaq <sundasr621@gmail.com>
2026-09-13 16:58:27 +0500

    env issues

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "*",
+    origin: [
+      "https://talkio-chat-cctyhkaa8-sundas-razzaq.vercel.app",
+      "http://localhost:5173"
+    ],
     credentials: true,
   },
   path: "/socket.io",
